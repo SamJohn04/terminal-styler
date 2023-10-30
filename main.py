d235cfcb00a889cc3061fe38138c15b80af0333f
SamJohn04 <samuel.john.codes@gmail.com>
2023-10-30 22:55:23 +0530

    refactor

--- main.py
+++ main.py
@@ -2,56 +2,83 @@ from src.style import Style
 import sys
 import csv
 
-style_codes = { style_code[0] : style_code[1] for style_code in list(csv.reader(open('res/style-codes.csv', 'r'))) }
+def get_input_output(args):
 
-if len(sys.argv) < 2:
-    print('Usage: python3 main.py <file> [output_file]')
-    exit(1)
+    if len(args) < 2:
+        print('Usage: python3 main.py <file> [output_file]')
+        exit(1)
 
-file_name = sys.argv[1]
+    if args[1] == '-h' or args[1] == '--help':
+        print('Usage: python3 main.py <file> [output_file]')
+        print('If output_file is not specified, the output will be saved in a file named styled-<file>')
+        exit(0)
+        
+    file_name = args[1]
+    input_file = open(file_name, 'r')
 
-input_file = open(file_name, 'r')
-if len(sys.argv) == 3:
-    output_file = open(sys.argv[2], 'w')
-else:
-    if file_name.__contains__('/'):
-        output_file = open(f'{file_name[:file_name.rindex("/") + 1]}styled-{file_name[file_name.rindex("/") + 1:]}', 'w')
+    if len(args) == 3:
+        output_file = open(args[2], 'w')
     else:
-        output_file = open(f'styled-{file_name}', 'w')
+        if file_name.__contains__('/'):
+            output_file = open(f'{file_name[:file_name.rindex("/") + 1]}styled-{file_name[file_name.rindex("/") + 1:]}', 'w')
+        else:
+            output_file = open(f'styled-{file_name}', 'w')
+    return input_file, output_file
+
+def get_style_codes():
+    return { style_code[0] : style_code[1] for style_code in csv.reader(open('res/style-codes.csv', 'r')) }
+
+def filter_exclaimation(output):
+    i = 0
+    while i < len(output):
+        if output[i] == '!':
+            if output[i-1] == '<' or output[i-2:i] == '</':
+                output = output[:i] + output[i+1:]
+                while output[i] == '!':
+                    i += 1
+        i += 1
+    return output
+
+def get_styles(input_text):
+    styles_text = input_text.split('.')
+    return [style.strip().upper() for style in styles_text if style.strip() != '']
+
+def write_output(output_file, output):
+    output_file.write(filter_exclaimation(output))
+
+style_codes = get_style_codes()
+
+input_file, output_file = get_input_output(sys.argv)
 
 input_text = input_file.read()
 
-stack = []
-output = ''
+styles_stack = []
 
 while input_text.__contains__('<console'):
     index = input_text.index('<console')
+
     if index != 0:
-        output += str(Style(input_text[:index], list(stack), style_codes))
+        write_output(output_file, str(Style(input_text[:index], list(styles_stack), style_codes)))
+
     input_text = input_text[index:]
+    
     index = input_text.index('>')
-    styles_text = input_text[9:index].split('.')
-    styles = [style.strip().upper() for style in styles_text if style.strip() != '']
+
+    styles = get_styles(input_text[9:index])
+    styles_stack.append(styles)
+    
     input_text = input_text[index+1:]
-    stack.append(styles)
-    while input_text.__contains__('</console>') and input_text.find('</console>') < (input_text.find('<console') if input_text.__contains__('<console') else len(input_text)):
-        index = input_text.index('</console>')
-        output += str(Style(input_text[:index], list(stack), style_codes))
-        input_text = input_text[index+10:]
-        stack.pop()
+    
+    while input_text.__contains__('</console') and input_text.find('</console') < (input_text.find('<console') if input_text.__contains__('<console') else len(input_text)):
+        index = input_text.index('</console')
+        
+        write_output(output_file, str(Style(input_text[:index], list(styles_stack), style_codes)))
+        
+        input_text = input_text[index:]
+        input_text = input_text[input_text.index('>') + 1:]
+        styles_stack.pop()
 
-output += input_text
-
-i = 0
-while i < len(output):
-    if output[i] == '!':
-        if output[i-1] == '<' or output[i-2:i] == '</':
-            output = output[:i] + output[i+1:]
-            while output[i] == '!':
-                i += 1
-    i += 1
-
-output_file.write(output)
+write_output(output_file, input_text)
 
 input_file.close()
 output_file.close()
--- src/style.py
+++ src/style.py
@@ -8,20 +8,26 @@ class Style:
                 self.styles.append(style)
             i += 1
         self.style_codes = style_codes
-    def __str__(self):
-        def hex_to_rgb(hex):
-            hex = hex.lstrip('#')
-            hlen = len(hex)
-            return tuple(int(hex[i:i+hlen//3], 16) for i in range(0, hlen, hlen//3))
 
+    def __str__(self):
         style_code = ''
         for style in self.styles:
-            if not style.__contains__('['):
-                style_code += '\u001b[' + self.style_codes.get(style, '0m')
-            else:
-                style_code += '\u001b[' + ('38' if style[:style.index('-')] == 'COLOR' else '48') + ';2;'
-                style = style[style.index('[') + 1:style.index(']')]
-                rgb = hex_to_rgb(style)
-                style_code += str(rgb[0]) + ";" + str(rgb[1]) + ";" + str(rgb[2]) + 'm'
-        default_style_code = '\u001b[0m'
+            style_code += '\u001b[' + self.get_style_code(style)
+        default_style_code = '\u001b[' + self.style_codes.get('DEFAULT', '0m')
         return f'{style_code}{self.string}{default_style_code}'
+    
+    def hex_to_rgb(self, hex):
+        hex = hex.lstrip('#')
+        hlen = len(hex)
+        return tuple(int(hex[i:i+hlen//3], 16) for i in range(0, hlen, hlen//3))
+    
+    def get_style_code(self, style):
+        if not style.__contains__('['):
+            return self.style_codes.get(style, '0m')
+        else:
+            style_code = ''
+            style_code += ('38' if style[:style.index('-')] == 'COLOR' else '48') + ';2;'
+            style = style[style.index('[') + 1:style.index(']')]
+            rgb = self.hex_to_rgb(style)
+            style_code += str(rgb[0]) + ";" + str(rgb[1]) + ";" + str(rgb[2]) + 'm'
+            return style_code
